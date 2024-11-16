7e4bd3e68b6c56d01a1ad30fbba4a5412a6c569d
Harry Dinh <thongdinh2004@icloud.com>
2024-11-15 20:57:38 -0500

    Battery timer feature fully implemented, see description for details
    Summary:
    - Implemented the battery timer feature
    - The timer will start at 100% and decrease by 1% every 2 QTimer seconds
    - The timer only start counting down when the power button is toggled to ON, simulating the device running and draining power from the battery
    - The timer won't run when the power button is set to OFF, meaning that the device is not in use and cannot drain the battery

--- Team32-3004-Project/Team32-3004-Project.pro
+++ Team32-3004-Project/Team32-3004-Project.pro
@@ -13,6 +13,7 @@ SOURCES += \
     mainwindow.cpp
 
 HEADERS += \
+    defs.h \
     mainwindow.h
 
 FORMS += \
--- Team32-3004-Project/mainwindow.cpp
+++ Team32-3004-Project/mainwindow.cpp
@@ -1,12 +1,49 @@
+#include <iostream>
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "defs.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow) {
+using namespace std;
+
+MainWindow::MainWindow(QWidget *parent): QMainWindow(parent), ui(new Ui::MainWindow) {
+    // Initialize all member variables
     ui->setupUi(this);
+    this->batteryTimer = new QTimer();
+    this->batteryPercentage = STARTING_BATTERY_LEVEL;
+    
+    // Connect the battery timer to the appropriate function
+    connect(this->batteryTimer, &QTimer::timeout, this, QOverload<>::of(&MainWindow::drainBattery));
+    
+    // Connect the power button to its respective function
+    connect(ui->powerButton, &QPushButton::clicked, this, &MainWindow::powerButtonPressed);
+    
+    // Set the initial display value for the battery indicator
+    ui->batteryIndicator->display(STARTING_BATTERY_LEVEL);
 }
 
 MainWindow::~MainWindow() {
     delete ui;
+    delete this->batteryTimer;
+}
+
+void MainWindow::drainBattery() {
+    cout << "drainBattery() called" << endl;
+    if (batteryPercentage > 0) {
+        --batteryPercentage;
+        ui->batteryIndicator->display(batteryPercentage);
+    } else {
+        batteryTimer->stop();       // Stop the timer when the battery level drops to 0
+    }
+}
+
+void MainWindow::powerButtonPressed() {
+    if (!ui->powerButton->isDefault()) {
+        ui->powerButton->setDefault(true);
+        ui->powerButton->setText("Power Off");
+        batteryTimer->start(2000);      // Drain the battery 1% every 2 seconds
+    } else {
+        ui->powerButton->setDefault(false);
+        ui->powerButton->setText("Power On");
+        batteryTimer->stop();           // Simulate the action of turning off the device when the power button is pressed again
+    }
 }
