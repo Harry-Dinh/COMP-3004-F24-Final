4c4610a859953c18d5db26ff49af7e940c7c590e
Leng Tan <letng27@gmail.com>
2024-11-17 00:57:07 -0500

    Add Profile constructor

--- Team32-3004-Project/profile.cpp
+++ Team32-3004-Project/profile.cpp
@@ -1,1 +1,16 @@
 #include "profile.h"
+
+Profile::Profile(int id, const QString &firstName, const QString &lastName,
+                 int weight, int height, const QDate &DOB, QString &country,
+                 QString &phone, QString &email, QString &password){
+    this->id = id;
+    this->firstName = firstName;
+    this->lastName = lastName;
+    this->weight = weight;
+    this->height = height;
+    this->DOB = DOB;
+    this->country = country;
+    this->phone = phone;
+    this->email = email;
+    this->password = password;
+}
