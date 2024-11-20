ea5cb03e9fb752684bb8876f630032bd52edd2eb
Clarke <clarkevandervaart@cmail.carleton.ca>
2024-11-19 20:00:08 -0500

    Update history.cpp

--- Team32-3004-Project/history.cpp
+++ Team32-3004-Project/history.cpp
@@ -45,7 +45,7 @@ bool history::addProfile(int pid, const QString& fname, const QString& lname, in
     raDoTechDB.transaction();
     QSqlQuery query;
 
-    query.prepare("INSERT INTO profiles (id, firstName, lastName, weight, height, DOB, country, phone, email, password) VALUES (:id, :firstName, :lastName, :weight, :height, :DOB, :country, :phone, :email, :password);");
+    query.prepare("INSERT OR IGNORE INTO profiles (id, firstName, lastName, weight, height, DOB, country, phone, email, password) VALUES (:id, :firstName, :lastName, :weight, :height, :DOB, :country, :phone, :email, :password);");
     query.bindValue(":id", pid);
     query.bindValue(":firstName", fname);
     query.bindValue(":lastName", lname);
@@ -66,7 +66,7 @@ bool history::addHealth(int mid, const QDateTime& time, QVector<int> healthData)
     raDoTechDB.transaction();
     QSqlQuery query;
 
-    query.prepare("INSERT INTO measurements (mId, date, m_1, m_2, m_3, m_4, m_5, m_6, m_7, m_8, m_9, m_10, m_11, m_12, m_13, m_14, m_15, m_16, m_17, m_18, m_19, m_20, m_21, m_22, m_23, m_24) VALUES (:mId, :date, :m_1, :m_2, :m_3, :m_4, :m_5, :m_6, :m_7, :m_8, :m_9, :m_10, :m_11, :m_12, :m_13, :m_14, :m_15, :m_16, :m_17, :m_18, :m_19, :m_20, :m_21, :m_22, :m_23, :m_24);");
+    query.prepare("INSERT OR IGNORE INTO measurements (mId, date, m_1, m_2, m_3, m_4, m_5, m_6, m_7, m_8, m_9, m_10, m_11, m_12, m_13, m_14, m_15, m_16, m_17, m_18, m_19, m_20, m_21, m_22, m_23, m_24) VALUES (:mId, :date, :m_1, :m_2, :m_3, :m_4, :m_5, :m_6, :m_7, :m_8, :m_9, :m_10, :m_11, :m_12, :m_13, :m_14, :m_15, :m_16, :m_17, :m_18, :m_19, :m_20, :m_21, :m_22, :m_23, :m_24);");
     query.bindValue(":mId", mid);
     query.bindValue(":date", time.toString("yyyy-MM-dd hh:mm"));
     query.bindValue(":m_1", healthData[0]);
