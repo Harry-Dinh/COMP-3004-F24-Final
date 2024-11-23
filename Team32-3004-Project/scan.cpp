ebdfc0c0a4a268e1209c0409f98de146543cf9eb
Harry Dinh <thongdinh2004@icloud.com>
2024-11-22 21:20:19 -0500

    Refactored the Scan class, read description
    - Refactored the scan class to include a bound checking function and a member Measurement variable
    Honestly after refactoring the Measurement class, I don't see the need for this class to be around anymore. But I'm still going to leave it here for now since it contains my implemented function.
    
    Asaad, since you're doing the Summary part, may I suggest you put my healthChecking() function into your class and replace the member Scan object with Measurement instead? After that, you can dispose of this Scan class

--- Team32-3004-Project/scan.cpp
+++ Team32-3004-Project/scan.cpp
@@ -1,5 +1,16 @@
 #include "scan.h"
+#include "defs.h"
 
 Scan::Scan(Measurement* measurement) {
     this->measurement = measurement;
+}
+
+int Scan::healthChecking(double valueToCheck) {
+    if (valueToCheck < HEALTHY_LOWER_BOUND) {
+        return -1;
+    } else if (valueToCheck > HEALTHY_UPPER_BOUND) {
+        return 1;
+    } else {
+        return 0;
+    }
 }
